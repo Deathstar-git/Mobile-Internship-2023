eb0d07e895d46d034a2ffdc5a5df2a9661dfe4c0
Timofey Chernenko <79025632112tch@gmail.com>
2023-03-20 23:43:16 +0700

    prev PR fix: Поправил зависимости и зарегистрировал роутер в App виджете

--- lib/infrastructure/common/injection_module.dart
+++ lib/infrastructure/common/injection_module.dart
@@ -1,21 +1,23 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
-import 'package:mobile_internship_1/application/auth_bloc/auth_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import '../../ui/navigation/routes.gr.dart';
 
 @module
 abstract class ServiceModule {
 
+  @lazySingleton
   @preResolve
   Future<SharedPreferences> get prefs => SharedPreferences.getInstance();
 
-  @singleton
+  @lazySingleton
   Dio get dio => Dio();
 
-  @singleton
+  @lazySingleton
   FlutterSecureStorage get storage => const FlutterSecureStorage();
 
-  @singleton
-  AuthBloc get authService => AuthBloc();
+  @lazySingleton
+  AppRouter get router => AppRouter();
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_internship_1/ui/navigation/routes.gr.dart';
 
 import 'injection.dart';
 
@@ -10,7 +11,7 @@ Future<void > main() async{
   await configureDependencies();
 
   return runZonedGuarded(() async {
-    runApp(const MyApp());
+    runApp(MyApp());
   }, (error, stack) {
     if (kDebugMode) {
       print(stack);
@@ -20,26 +21,20 @@ Future<void > main() async{
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
+  final _appRouter = getIt<AppRouter>();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
+
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        routerDelegate: _appRouter.delegate(),
+        routeInformationParser: _appRouter.defaultRouteParser(),
     );
   }
 }
