f19ec3b787c9ecc69119d12ecb45455f8847bf21
Timofey Chernenko <79025632112tch@gmail.com>
2023-03-25 15:00:45 +0700

    Добавил svg лого в AppBar

--- lib/ui/features/main_page/main_page.dart
+++ lib/ui/features/main_page/main_page.dart
@@ -6,6 +6,7 @@ import 'package:mobile_internship_1/ui/features/main_page/widgets/marginality_ch
 import 'package:mobile_internship_1/ui/features/main_page/widgets/marginality_list.dart';
 import '../../../application/marginality_choice_bloc/marginality_choice_bloc.dart';
 import '../../../injection.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 //Главная страница
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -14,15 +15,27 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: const Center(
-              child: Text('Marginality',
-                  style: TextStyle(fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: AppColors.gray_1)
-              )
-          ),
           backgroundColor: AppColors.black_1,
-      ),
+          title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:[
+                  SvgPicture.asset(
+                      'assets/svg/logo.svg',
+                      semanticsLabel: 'Marginality Logo'
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child:  Text('Marginality',
+                        style: TextStyle(fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.gray_1)
+                    )
+                  )
+                 
+                ]
+              ),
+          ),
+
       body: MultiBlocProvider(
         providers: [
           BlocProvider<MarginalityBloc>(
